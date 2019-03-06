4d80ae51a75f9fa7ce00d43233f286cd6123378b
Manuel Schneckenreither <manuel.schneckenreither@uibk.ac.at>
2019-03-06 19:32:29 +0100

    added open AI support

--- examples/GymOpenAI.hs
+++ examples/GymOpenAI.hs
@@ -22,59 +22,46 @@
 module Main where
 
 import           ML.BORL
+import           ML.Gym
+
 
 import           Helper
 
-import           Control.Arrow            (first, second)
-import           Control.DeepSeq          (NFData)
+import           Control.Arrow          (first, second)
+import           Control.DeepSeq        (NFData)
+import qualified Control.Exception      as E
 import           Control.Lens
-import           Control.Lens             (set, (^.))
-import           Control.Monad            (forM, forM_, void)
-import           Control.Monad            (foldM, unless, when)
-import           Control.Monad.IO.Class   (liftIO)
-import           Data.List                (genericLength)
-import qualified Data.Text                as T
+import           Control.Lens           (set, (^.))
+import           Control.Monad          (foldM_, forM, forM_, void)
+import           Control.Monad          (foldM, unless, when)
+import           Control.Monad.IO.Class (liftIO)
+import           Data.List              (genericLength)
+import qualified Data.Text              as T
 import           GHC.Generics
-import           GHC.Int                  (Int32, Int64)
+import           GHC.Int                (Int32, Int64)
 import           Grenade
 import           System.Exit
 import           System.IO
 import           System.Random
 
-import qualified Control.Exception        as E
-import qualified CPython                  as Py
-import qualified CPython.Constants        as Py
-import qualified CPython.Protocols.Object as Py
-import qualified CPython.Types            as Py
-import qualified CPython.Types.Exception  as Py
-import qualified CPython.Types.Module     as Py
-import qualified CPython.Types.Module     as Py
-
-import qualified TensorFlow.Build         as TF (addNewOp, evalBuildT, explicitName, opDef,
-                                                 opDefWithName, opType, runBuildT,
-                                                 summaries)
-import qualified TensorFlow.Core          as TF hiding (value)
--- import qualified TensorFlow.GenOps.Core                         as TF (square)
-import qualified TensorFlow.GenOps.Core   as TF (abs, add, approximateEqual,
-                                                 approximateEqual, assign, cast,
-                                                 getSessionHandle, getSessionTensor,
-                                                 identity', lessEqual, matMul, mul,
-                                                 readerSerializeState, relu, relu', shape,
-                                                 square, sub, tanh, tanh', truncatedNormal)
-import qualified TensorFlow.Minimize      as TF
--- import qualified TensorFlow.Ops                                 as TF (abs, add, assign,
---                                                                        cast, identity',
---                                                                        matMul, mul, relu,
---                                                                        sub,
---                                                                        truncatedNormal)
-import qualified TensorFlow.Ops           as TF (initializedVariable, initializedVariable',
-                                                 placeholder, placeholder', reduceMean,
-                                                 reduceSum, restore, save, scalar, vector,
-                                                 zeroInitializedVariable,
-                                                 zeroInitializedVariable')
-import qualified TensorFlow.Tensor        as TF (Ref (..), collectAllSummaries,
-                                                 tensorNodeName, tensorRefFromName,
-                                                 tensorValueFromName)
+import qualified TensorFlow.Build       as TF (addNewOp, evalBuildT, explicitName, opDef,
+                                               opDefWithName, opType, runBuildT, summaries)
+import qualified TensorFlow.Core        as TF hiding (value)
+import qualified TensorFlow.GenOps.Core as TF (abs, add, approximateEqual,
+                                               approximateEqual, assign, cast,
+                                               getSessionHandle, getSessionTensor,
+                                               identity', lessEqual, matMul, mul,
+                                               readerSerializeState, relu, relu', shape,
+                                               square, sub, tanh, tanh', truncatedNormal)
+import qualified TensorFlow.Minimize    as TF
+import qualified TensorFlow.Ops         as TF (initializedVariable, initializedVariable',
+                                               placeholder, placeholder', reduceMean,
+                                               reduceSum, restore, save, scalar, vector,
+                                               zeroInitializedVariable,
+                                               zeroInitializedVariable')
+import qualified TensorFlow.Tensor      as TF (Ref (..), collectAllSummaries,
+                                               tensorNodeName, tensorRefFromName,
+                                               tensorValueFromName)
 
 
 maxX,maxY :: Int
@@ -84,73 +71,73 @@ maxY = 4                        -- [0..maxY]
 
 type NN = Network  '[ FullyConnected 2 20, Relu, FullyConnected 20 10, Relu, FullyConnected 10 10, Relu, FullyConnected 10 5, Tanh] '[ 'D1 2, 'D1 20, 'D1 20, 'D1 10, 'D1 10, 'D1 10, 'D1 10, 'D1 5, 'D1 5]
 
-nnConfig :: NNConfig St
-nnConfig = NNConfig
-  { _toNetInp             = netInp
+nnConfig :: Gym -> NNConfig St
+nnConfig gym = NNConfig
+  { _toNetInp             = netInp gym
   , _replayMemoryMaxSize  = 10000
   , _trainBatchSize       = 32
   , _learningParams       = LearningParameters 0.01 0.9 0.0001
-  , _prettyPrintElems     = [minBound .. maxBound] :: [St]
-  , _scaleParameters      = scalingByMaxAbsReward False 6
+  , _prettyPrintElems     = ppSts
+  , _scaleParameters      = scalingByMaxAbsReward False 1
   , _updateTargetInterval = 10000
   , _trainMSEMax          = 0.00125
   }
 
-netInp :: St -> [Double]
-netInp st = [scaleNegPosOne (0, fromIntegral maxX) $ fromIntegral $ fst (getCurrentIdx st),
-             scaleNegPosOne (0, fromIntegral maxY) $ fromIntegral $ snd (getCurrentIdx st)]
+  where range = getGymRangeFromSpace $ observationSpace gym
+        (lows, highs) = gymRangeToDoubleLists range
+        vals = zipWith (\lo hi -> [lo, lo+(hi-lo)/3..hi]) lows highs
+        ppSts = take 1000 $ combinations vals
+
+combinations :: [[a]] -> [[a]]
+combinations []       = []
+combinations [xs] = map return xs
+combinations (xs:xss) = concatMap (\x -> map (x:) ys) xs
+  where ys = combinations xss
+
+type St = [Double]
 
 
-modelBuilder :: (TF.MonadBuild m) => m TensorflowModel
-modelBuilder =
+netInp :: Gym -> St -> [Double]
+netInp gym = zipWith3 (curry scaleNegPosOne) lows highs
+  where range = getGymRangeFromSpace $ observationSpace gym
+        (lows, highs) = gymRangeToDoubleLists range
+
+  -- [scaleNegPosOne (0, fromIntegral maxX) $ fromIntegral $ fst (getCurrentIdx st),
+  --   scaleNegPosOne (0, fromIntegral maxY) $ fromIntegral $ snd (getCurrentIdx st)]
+
+
+modelBuilder :: (TF.MonadBuild m) => Integer -> Integer -> m TensorflowModel
+modelBuilder nrInp nrOut =
   buildModel $
-  inputLayer1D (genericLength (netInp initState)) >> fullyConnected1D 10 TF.relu' >> fullyConnected1D 7 TF.relu' >> fullyConnected1D (genericLength actions) TF.tanh' >>
+  inputLayer1D (fromIntegral nrInp) >> fullyConnected1D 10 TF.relu' >> fullyConnected1D 7 TF.relu' >> fullyConnected1D (fromIntegral nrOut) TF.tanh' >>
   trainingByAdam1DWith TF.AdamConfig {TF.adamLearningRate = 0.001, TF.adamBeta1 = 0.9, TF.adamBeta2 = 0.999, TF.adamEpsilon = 1e-8}
 
-onException :: Py.Exception -> IO a
-onException exc = Py.print (Py.exceptionValue exc) stdout >> exitFailure
+action :: Gym -> Integer -> Action St
+action gym idx = flip Action (T.pack $ show idx) $ \_ -> do
+  res <- stepGym gym idx
+  obs <- if episodeDone res
+         then resetGym gym
+         else return $ observation res
+  return (reward res, gymObservationToDoubleList obs)
 
-python :: IO a -> IO a
-python = E.handle onException
 
-textOrNone :: Py.SomeObject -> IO (Maybe T.Text)
-textOrNone obj = do
-	isNone <- Py.isNone obj
-	if isNone
-		then return Nothing
-		else do
-			Just cast <- Py.cast obj
-			Just `fmap` Py.fromUnicode cast
 main :: IO ()
 main = do
-  Py.initialize
-  Py.setArgv "gym-py" []
-  gym <- python $ Py.importModule "gym"
-  pyName <- Py.toUnicode "CartPole-v0"
-  env <- python $ Py.callMethodArgs gym "make" [Py.toObject pyName]
-  observation <- python $ Py.callMethodArgs env "reset" []
-
-  forM_ ([1..100] :: [Int]) $ \x -> do
-    void $ python $ Py.callMethodArgs env "render" []
-    void $ python $ Py.print observation stdout
-    actionSpace <- Py.getAttribute env =<< Py.toUnicode "action_space"
-    sample <- python $ Py.callMethodArgs actionSpace "sample" []
-    void $ python $ Py.callMethodArgs env "step" [Py.toObject sample]
-    putStrLn $ "Step" ++ show x
-    jsonActionSpace <- python $ Py.callMethodArgs actionSpace "__repr__" []
-    textOrNone json >>= printActionSpace
+  (obs, gym) <- initGym "CartPole-v0"
+  let inputNodes = dimension (observationSpace gym)
+      actionNodes = dimension (actionSpace gym)
+      initState = gymObservationToDoubleList obs
+      actions = map (action gym) [0..actionNodes-1]
 
 
   nn <- randomNetworkInitWith UniformInit :: IO NN
   -- rl <- mkBORLUnichainGrenade initState actions actFilter params decay nn nnConfig
-  -- rl <- mkBORLUnichainTensorflow initState actions actFilter params decay modelBuilder nnConfig
-  let rl = mkBORLUnichainTabular initState actions actFilter params decay
+  rl <- mkBORLUnichainTensorflow initState actions actFilter params decay (modelBuilder inputNodes actionNodes) (nnConfig gym)
+  -- let rl = mkBORLUnichainTabular initState actions actFilter params decay
   askUser True usage cmds rl   -- maybe increase learning by setting estimate of rho
 
-  where cmds = zipWith3 (\n (s,a) na -> (s, (n, Action a na))) [0..] [("i",moveUp),("j",moveDown), ("k",moveLeft), ("l", moveRight) ] (tail names)
-        usage = [("i","Move up") , ("j","Move left") , ("k","Move down") , ("l","Move right")]
-
-names = ["random", "up   ", "down ", "left ", "right"]
+  where cmds = []
+        usage = []
 
 -- | BORL Parameters.
 params :: Parameters
@@ -189,91 +176,5 @@ decay t (psiRhoOld, psiVOld, psiWOld) (psiRhoNew, psiVNew, psiWNew) p@(Parameter
     f = max 0.01
 
 
-initState :: St
-initState = fromIdx (2,2)
-
-
--- State
-newtype St = St [[Integer]] deriving (Eq,NFData,Generic)
-
-instance Ord St where
-  x <= y = fst (getCurrentIdx x) < fst (getCurrentIdx y) || (fst (getCurrentIdx x) == fst (getCurrentIdx y) && snd (getCurrentIdx x) < snd (getCurrentIdx y))
-
-instance Show St where
-  show xs = show (getCurrentIdx xs)
-
-instance Enum St where
-  fromEnum st = let (x,y) = getCurrentIdx st
-                in x * (maxX + 1) + y
-  toEnum x = fromIdx (x `div` (maxX+1), x `mod` (maxX+1))
-
-instance Bounded St where
-  minBound = fromIdx (0,0)
-  maxBound = fromIdx (maxX, maxY)
-
-
--- Actions
-actions :: [Action St]
-actions = zipWith Action
-  (map goalState [moveRand, moveUp, moveDown, moveLeft, moveRight])
-  names
-
 actFilter :: St -> [Bool]
-actFilter st | st == fromIdx (0,2) = True : repeat False
-actFilter _  = False : repeat True
-
-
-moveRand :: St -> IO (Reward, St)
-moveRand = moveUp
-
-
-goalState :: (St -> IO (Reward, St)) -> St -> IO (Reward, St)
-goalState f st = do
-  x <- randomRIO (0, maxX :: Int)
-  y <- randomRIO (0, maxY :: Int)
-  r <- randomRIO (0, 8 :: Double)
-  let stepRew = first (+ r)
-  case getCurrentIdx st of
-    -- (0, 1) -> return [(1, (10, fromIdx (x,y)))]
-    -- (0, 2) -> return (10, fromIdx (4,2)) -- (x,y))
-    (0, 2) -> return (10, fromIdx (x,y))
-    -- (0, 3) -> return [(1, (5, fromIdx (x,y)))]
-    _      -> stepRew <$> f st
-
-
-moveUp :: St -> IO (Reward,St)
-moveUp st
-    | m == 0 = return (-1, st)
-    | otherwise = return (0, fromIdx (m-1,n))
-  where (m,n) = getCurrentIdx st
-
-moveDown :: St -> IO (Reward,St)
-moveDown st
-    | m == maxX = return (-1, st)
-    | otherwise = return (0, fromIdx (m+1,n))
-  where (m,n) = getCurrentIdx st
-
-moveLeft :: St -> IO (Reward,St)
-moveLeft st
-    | n == 0 = return (-1, st)
-    | otherwise = return (0, fromIdx (m,n-1))
-  where (m,n) = getCurrentIdx st
-
-moveRight :: St -> IO (Reward,St)
-moveRight st
-    | n == maxY = return (-1, st)
-    | otherwise = return (0, fromIdx (m,n+1))
-  where (m,n) = getCurrentIdx st
-
-
--- Conversion from/to index for state
-
-fromIdx :: (Int, Int) -> St
-fromIdx (m,n) = St $ zipWith (\nr xs -> zipWith (\nr' ys -> if m == nr && n == nr' then 1 else 0) [0..] xs) [0..] base
-  where base = replicate 5 [0,0,0,0,0]
-
-
-getCurrentIdx :: St -> (Int,Int)
-getCurrentIdx (St st) = second (fst . head . filter ((==1) . snd)) $
-  head $ filter ((1 `elem`) . map snd . snd) $
-  zip [0..] $ map (zip [0..]) st
+actFilter _  = repeat True
