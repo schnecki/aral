54eb7c4e92fbf8f7861834fec14d9d2ce787b4ee
Manuel Schneckenreither <manuel.schneckenreither@uibk.ac.at>
2020-03-10 15:06:31 +0100

    Objective Test using GridworldStep

---
+++ examples/GridworldStep.hs
@@ -0,0 +1,311 @@
+{-# LANGUAGE DataKinds                  #-}
+{-# LANGUAGE DeriveAnyClass             #-}
+{-# LANGUAGE DeriveGeneric              #-}
+{-# LANGUAGE FlexibleInstances          #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE InstanceSigs               #-}
+{-# LANGUAGE OverloadedLists            #-}
+{-# LANGUAGE OverloadedStrings          #-}
+{-# LANGUAGE TupleSections              #-}
+{-# LANGUAGE TypeFamilies               #-}
+
+-- | Gridworld example that simply returns 1 for every action, except when in the goal state where it returns 0. This is
+-- simply used to test the minimisation objective.
+module Main where
+
+import           ML.BORL                  as B
+import           SolveLp
+
+import           Experimenter
+
+import           Helper
+
+import           Control.Arrow            (first, second, (***))
+import           Control.DeepSeq          (NFData)
+import           Control.Lens
+import           Control.Lens             (set, (^.))
+import           Control.Monad            (foldM, liftM, unless, when)
+import           Control.Monad.IO.Class   (liftIO)
+import           Data.Function            (on)
+import           Data.List                (genericLength, groupBy, sort, sortBy)
+import qualified Data.Map.Strict          as M
+import           Data.Serialize
+import           Data.Singletons.TypeLits hiding (natVal)
+import qualified Data.Text                as T
+import           Data.Text.Encoding       as E
+import           GHC.Generics
+import           GHC.Int                  (Int32, Int64)
+import           GHC.TypeLits
+import           Grenade
+import           System.IO
+import           System.Random
+
+
+import qualified TensorFlow.Build         as TF (addNewOp, evalBuildT, explicitName, opDef,
+                                                 opDefWithName, opType, runBuildT,
+                                                 summaries)
+import qualified TensorFlow.Core          as TF hiding (value)
+-- import qualified TensorFlow.GenOps.Core                         as TF (square)
+import qualified TensorFlow.GenOps.Core   as TF (abs, add, approximateEqual,
+                                                 approximateEqual, assign, cast,
+                                                 getSessionHandle, getSessionTensor,
+                                                 identity', lessEqual, matMul, mul,
+                                                 readerSerializeState, relu, relu', shape,
+                                                 square, sub, tanh, tanh', truncatedNormal)
+import qualified TensorFlow.Minimize      as TF
+-- import qualified TensorFlow.Ops                                 as TF (abs, add, assign,
+--                                                                        cast, identity',
+--                                                                        matMul, mul, relu,
+--                                                                        sub,
+--                                                                        truncatedNormal)
+import qualified TensorFlow.Ops           as TF (initializedVariable, initializedVariable',
+                                                 placeholder, placeholder', reduceMean,
+                                                 reduceSum, restore, save, scalar, vector,
+                                                 zeroInitializedVariable,
+                                                 zeroInitializedVariable')
+import qualified TensorFlow.Session       as TF
+import qualified TensorFlow.Tensor        as TF (Ref (..), collectAllSummaries,
+                                                 tensorNodeName, tensorRefFromName,
+                                                 tensorValueFromName)
+
+
+maxX, maxY, goalX, goalY :: Int
+maxX = 4                        -- [0..maxX]
+maxY = 4                        -- [0..maxY]
+goalX = 0
+goalY = 0
+
+instance RewardFuture St where
+  type StoreType St = ()
+
+nnConfig :: NNConfig
+nnConfig =
+  NNConfig
+    { _replayMemoryMaxSize = 10000
+    , _trainBatchSize = 8
+    , _grenadeLearningParams = LearningParameters 0.01 0.0 0.0001
+    , _learningParamsDecay = ExponentialDecay Nothing 0.05 100000
+    , _prettyPrintElems = map netInp ([minBound .. maxBound] :: [St])
+    , _scaleParameters = scalingByMaxAbsReward False 6
+    , _stabilizationAdditionalRho = 0.5
+    , _stabilizationAdditionalRhoDecay = ExponentialDecay Nothing 0.05 100000
+    , _updateTargetInterval = 1
+    , _updateTargetIntervalDecay = NoDecay
+    , _trainMSEMax = Nothing -- Just 0.03
+    , _setExpSmoothParamsTo1 = True
+    }
+
+
+-- | BORL Parameters.
+params :: ParameterInitValues
+params =
+  Parameters
+    { _alpha               = 0.01
+    , _beta                = 0.01
+    , _delta               = 0.005
+    , _gamma               = 0.01
+    , _epsilon             = 0.25
+    , _explorationStrategy = EpsilonGreedy -- SoftmaxBoltzmann 10 -- EpsilonGreedy
+    , _exploration         = 1.0
+    , _learnRandomAbove    = 1.5
+    , _zeta                = 0.03
+    , _xi                  = 0.005
+    , _disableAllLearning  = False
+    -- ANN
+    , _alphaANN            = 0.5 -- only used for multichain
+    , _betaANN             = 0.5
+    , _deltaANN            = 0.5
+    , _gammaANN            = 0.5
+    }
+
+-- | Decay function of parameters.
+decay :: Decay
+decay =
+  decaySetupParameters
+    Parameters
+      { _alpha            = ExponentialDecay (Just 1e-5) 0.5 50000  -- 5e-4
+      , _beta             = ExponentialDecay (Just 1e-4) 0.5 50000
+      , _delta            = ExponentialDecay (Just 5e-4) 0.5 50000
+      , _gamma            = ExponentialDecay (Just 1e-3) 0.5 50000 -- 1e-3
+      , _zeta             = ExponentialDecay (Just 0) 0.5 50000
+      , _xi               = NoDecay
+      -- Exploration
+      , _epsilon          = NoDecay -- ExponentialDecay (Just 5.0) 0.5 150000
+      , _exploration      = ExponentialDecay (Just 0.01) 0.50 30000
+      , _learnRandomAbove = NoDecay
+      -- ANN
+      , _alphaANN         = ExponentialDecay Nothing 0.75 150000
+      , _betaANN          = ExponentialDecay Nothing 0.75 150000
+      , _deltaANN         = ExponentialDecay Nothing 0.75 150000
+      , _gammaANN         = ExponentialDecay Nothing 0.75 150000
+      }
+
+
+initVals :: InitValues
+initVals = InitValues 1 10 0 0 0 0
+
+main :: IO ()
+main = usermode
+
+
+mRefState :: Maybe (St, ActionIndex)
+mRefState = Nothing
+-- mRefState = Just (fromIdx (goalX, goalY), 0)
+
+alg :: Algorithm St
+alg =
+
+  -- AlgBORLVOnly ByStateValues Nothing
+        -- AlgDQN 0.99  EpsilonSensitive
+        -- AlgDQN 0.50  EpsilonSensitive            -- does work
+        -- algDQNAvgRewardFree
+        AlgDQNAvgRewAdjusted Nothing 0.8 0.99 ByStateValues
+  -- AlgBORL 0.5 0.8 ByStateValues mRefState
+
+usermode :: IO ()
+usermode = do
+
+  -- Approximate all fucntions using a single neural network
+  rl <-
+    case alg of
+      AlgBORL{} -> (randomNetworkInitWith UniformInit :: IO NNCombined) >>= \nn -> mkUnichainGrenadeCombinedNet alg initState netInp actions actFilter params decay nn nnConfig (Just initVals)
+      AlgDQNAvgRewAdjusted {} -> (randomNetworkInitWith UniformInit :: IO NNCombinedAvgFree) >>= \nn -> mkUnichainGrenadeCombinedNet alg initState netInp actions actFilter params decay nn nnConfig (Just initVals)
+      _ ->  (randomNetworkInitWith UniformInit :: IO NN) >>= \nn -> mkUnichainGrenadeCombinedNet alg initState netInp actions actFilter params decay nn nnConfig (Just initVals)
+
+  -- Use an own neural network for every function to approximate
+  -- rl <- (randomNetworkInitWith UniformInit :: IO NN) >>= \nn -> mkUnichainGrenade alg initState netInp actions actFilter params decay nn nnConfig (Just initVals)
+  -- rl <- mkUnichainTensorflow alg initState netInp actions actFilter params decay modelBuilder nnConfig  (Just initVals)
+  -- rl <- mkUnichainTensorflowCombinedNet alg initState netInp actions actFilter params decay modelBuilder nnConfig (Just initVals)
+
+  -- Use a table to approximate the function (tabular version)
+  let rl = mkUnichainTabular alg initState tblInp actions actFilter params decay (Just initVals)
+
+  askUser mInverseSt True usage cmds [] (flipObjective rl)
+  where
+    cmds =
+      zipWith3
+        (\n (s, a) na -> (s, (n, Action a na)))
+        [0 ..]
+        [("i", goalState moveUp), ("j", goalState moveDown), ("k", goalState moveLeft), ("l", goalState moveRight)]
+        (tail names)
+    usage = [("i", "Move up"), ("j", "Move left"), ("k", "Move down"), ("l", "Move right")]
+
+
+type NN = Network  '[ FullyConnected 2 20, Relu, FullyConnected 20 10, Relu, FullyConnected 10 10, Relu, FullyConnected 10 5, Tanh] '[ 'D1 2, 'D1 20, 'D1 20, 'D1 10, 'D1 10, 'D1 10, 'D1 10, 'D1 5, 'D1 5]
+type NNCombined = Network  '[ FullyConnected 2 20, Relu, FullyConnected 20 40, Relu, FullyConnected 40 40, Relu, FullyConnected 40 30, Tanh] '[ 'D1 2, 'D1 20, 'D1 20, 'D1 40, 'D1 40, 'D1 40, 'D1 40, 'D1 30, 'D1 30]
+type NNCombinedAvgFree = Network  '[ FullyConnected 2 20, Relu, FullyConnected 20 10, Relu, FullyConnected 10 10, Relu, FullyConnected 10 10, Tanh] '[ 'D1 2, 'D1 20, 'D1 20, 'D1 10, 'D1 10, 'D1 10, 'D1 10, 'D1 10, 'D1 10]
+
+modelBuilder :: (TF.MonadBuild m) => Int64 -> m TensorflowModel
+modelBuilder colOut =
+  buildModel $
+  inputLayer1D inpLen >> fullyConnected [20] TF.relu' >> fullyConnected [10] TF.relu' >> fullyConnected [10] TF.relu' >> fullyConnected [genericLength actions, colOut] TF.tanh' >>
+  trainingByAdamWith TF.AdamConfig {TF.adamLearningRate = 0.001, TF.adamBeta1 = 0.9, TF.adamBeta2 = 0.999, TF.adamEpsilon = 1e-8}
+  -- trainingByGradientDescent 0.01
+  where inpLen = genericLength (netInp initState)
+
+
+netInp :: St -> [Double]
+netInp st = [scaleNegPosOne (0, fromIntegral maxX) $ fromIntegral $ fst (getCurrentIdx st), scaleNegPosOne (0, fromIntegral maxY) $ fromIntegral $ snd (getCurrentIdx st)]
+
+tblInp :: St -> [Double]
+tblInp st = [fromIntegral $ fst (getCurrentIdx st), fromIntegral $ snd (getCurrentIdx st)]
+
+names = ["random", "up   ", "down ", "left ", "right"]
+
+initState :: St
+initState = fromIdx (maxX,maxY)
+
+goal :: St
+goal = fromIdx (goalX, goalY)
+
+-- State
+newtype St = St [[Integer]] deriving (Eq, NFData, Generic, Serialize)
+
+instance Ord St where
+  x <= y = fst (getCurrentIdx x) < fst (getCurrentIdx y) || (fst (getCurrentIdx x) == fst (getCurrentIdx y) && snd (getCurrentIdx x) < snd (getCurrentIdx y))
+
+instance Show St where
+  show xs = show (getCurrentIdx xs)
+
+instance Enum St where
+  fromEnum st = let (x,y) = getCurrentIdx st
+                in x * (maxX + 1) + y
+  toEnum x = fromIdx (x `div` (maxX+1), x `mod` (maxX+1))
+
+instance Bounded St where
+  minBound = fromIdx (0,0)
+  maxBound = fromIdx (maxX, maxY)
+
+
+-- Actions
+actions :: [Action St]
+actions = zipWith Action
+  (map goalState [moveRand, moveUp, moveDown, moveLeft, moveRight])
+  names
+
+actFilter :: St -> [Bool]
+actFilter st
+  | st == fromIdx (goalX, goalY) = True : repeat False
+actFilter _  = False : repeat True
+
+
+moveRand :: St -> IO (Reward St, St, EpisodeEnd)
+moveRand = moveUp
+
+
+goalState :: (St -> IO (Reward St, St, EpisodeEnd)) -> St -> IO (Reward St, St, EpisodeEnd)
+goalState f st = do
+  x <- randomRIO (0, maxX :: Int)
+  y <- randomRIO (0, maxY :: Int)
+  r <- randomRIO (0, 8 :: Double)
+  let stepRew (Reward re, s, e) = (Reward $ re + r, s, e)
+  case getCurrentIdx st of
+    (x', y')
+      | x' == goalX && y' == goalY -> return (Reward 0, fromIdx (x, y), False)
+    _ -> stepRew <$> f st
+
+moveUp :: St -> IO (Reward St,St, EpisodeEnd)
+moveUp st
+    | m == 0 = return (Reward 1, st, False)
+    | otherwise = return (Reward 1, fromIdx (m-1,n), False)
+  where (m,n) = getCurrentIdx st
+
+moveDown :: St -> IO (Reward St,St, EpisodeEnd)
+moveDown st
+    | m == maxX = return (Reward 1, st, False)
+    | otherwise = return (Reward 1, fromIdx (m+1,n), False)
+  where (m,n) = getCurrentIdx st
+
+moveLeft :: St -> IO (Reward St,St, EpisodeEnd)
+moveLeft st
+    | n == 0 = return (Reward 1, st, False)
+    | otherwise = return (Reward 1, fromIdx (m,n-1), False)
+  where (m,n) = getCurrentIdx st
+
+moveRight :: St -> IO (Reward St,St, EpisodeEnd)
+moveRight st
+    | n == maxY = return (Reward 1, st, False)
+    | otherwise = return (Reward 1, fromIdx (m,n+1), False)
+  where (m,n) = getCurrentIdx st
+
+
+-- Conversion from/to index for state
+
+fromIdx :: (Int, Int) -> St
+fromIdx (m,n) = St $ zipWith (\nr xs -> zipWith (\nr' ys -> if m == nr && n == nr' then 1 else 0) [0..] xs) [0..] base
+  where base = replicate 5 [0,0,0,0,0]
+
+
+allStateInputs :: M.Map [Double] St
+allStateInputs = M.fromList $ zip (map netInp [minBound..maxBound]) [minBound..maxBound]
+
+mInverseSt :: Maybe (NetInputWoAction -> Maybe (Either String St))
+mInverseSt = Just $ \xs -> return <$> M.lookup xs allStateInputs
+
+getCurrentIdx :: St -> (Int,Int)
+getCurrentIdx (St st) =
+  second (fst . head . filter ((==1) . snd)) $
+  head $ filter ((1 `elem`) . map snd . snd) $
+  zip [0..] $ map (zip [0..]) st
+
+
