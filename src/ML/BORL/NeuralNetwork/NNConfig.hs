0242407c20058f260d5f882bb9753efcdda0e9cf
Manuel Schneckenreither <manuel.schneckenreither@uibk.ac.at>
2020-05-18 21:56:28 +0200

    updated comment

--- src/ML/BORL/NeuralNetwork/NNConfig.hs
+++ src/ML/BORL/NeuralNetwork/NNConfig.hs
@@ -42,7 +42,7 @@ data NNConfig =
     , _prettyPrintElems                :: ![NetInputWoAction]      -- ^ Sample input features for printing.
     , _scaleParameters                 :: !ScalingNetOutParameters -- ^ How to scale the output to the original range.
     , _cropTrainMaxValScaled           :: Maybe Float              -- ^ Crop the min and max of the learned scaled values, e.g. Just 0.98 -> Crops all values to (-0.98, 0.98) prior to learning. Useful
-                                                                   -- when using Tanh as output activation.
+                                                                   -- when using Tanh as output activation. Currently for Grenade only (as this part is in the sublibrary higher-level-tensorflow)!
     , _grenadeDropoutFlipActivePeriod  :: !Int                     -- ^ Flip dropout active/inactive state every X periods.
     , _grenadeDropoutOnlyInactiveAfter :: !Int                     -- ^ Keep dropout inactive when reaching the given number of periods. Set to 0 to inactive dropout active state flipping!
     , _updateTargetInterval            :: !Int                     -- ^ After how many steps should the target network be replaced by the worker?
