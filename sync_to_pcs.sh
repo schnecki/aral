7fb34dabdd4199570b43ec00e95fe83a2f6aa45b
Manuel Schneckenreither <manuel.schneckenreither@uibk.ac.at>
2022-10-03 13:29:53 +0200

    update sync

--- sync_to_pcs.sh
+++ sync_to_pcs.sh
@@ -31,22 +31,4 @@ for PC in $PCS; do
         fi
     done
 
-    # First sync models and data from other PC to here
-    rsync -tar -zz --force --exclude=*.bak $USER@$PC:$DIR/models/ $DIR/models/
-    rsync -tar -zz --force --exclude=*.bak $USER@$PC:$DIR/data/ $DIR/data/
-
-    # Sync aral-trader
-    echo "SYNCING TO $USER@$PC:$DIR"
-    rsync -tar -zz --del --force --exclude=.git --exclude=data.new --exclude=results --exclude=ordering --exclude=orders --exclude=prediction --exclude=aral-trader.prof --exclude=aral-trader.hp --exclude=*.bak --exclude=tmp/ --exclude=*.log --exclude=saved.bin --exclude=.stack-work --exclude=.stack-work.prof --exclude=state* --exclude=costs --exclude=plts --exclude=.state* --exclude=psiValues --exclude=.psiValues  --exclude=episodeLength --exclude=queueLength --exclude=reward* $DIR $USER@$PC:$DIR
-    if [ $? -ne 0 ]; then
-        ssh -t $USER@$PC "mkdir -p $DIR 1>/dev/null"
-        rsync -tar -zz --del --force --exclude=.git --exclude=data.new --exclude=.stack-work  --exclude=ordering --exclude=orders --exclude=state*  --exclude=costs --exclude=plts --exclude=.state* --exclude=psiValues --exclude=.psiValues --exclude=episodeLength --exclude=queueLength --exclude=reward* $DIR $USER@$PC:$DIR/
-    fi
-    echo "Synced data via rsync. Result $?"
-    if [ $? -eq 0 ]; then
-        printf "Synced to $PC"
-    else
-        echo "Something went wrong while syncing to $PC. Check the connection"
-    fi
-
 done
